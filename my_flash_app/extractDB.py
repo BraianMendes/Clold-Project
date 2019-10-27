5327ba70e490b2cbdca40701651c2fbf1a8873c8
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-10-27 13:11:56 -0300

    Server Python quase finalizado, resta rodar o template

--- my_flash_app/extractDB.py
+++ my_flash_app/extractDB.py
@@ -1,6 +1,26 @@
-from flask import Flask
-app = Flask(__name__)
+ #!/usr/bin/python3
+import pymysql
 
+from flask import Flask, render_template
+app = Flask(__name__)
+class Database:
+    def __init__(self):
+        host = "localhost"
+        user = "root"
+        password = "password"
+        db = "manuelDB"
+        self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.
+                                   DictCursor)
+        self.cur = self.con.cursor()
+    def Idoso(self):
+        self.cur.execute("SELECT nomeIdoso, loginIdoso FROM Idoso WHERE idadeIdoso = 70")
+        result = self.cur.fetchall()
+        return result
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def Idoso():
+    def db_query():
+        db = Database()
+        oldAge = db.Idoso()
+        return oldAge
+    res = db_query()
+    return render_template('./idosos.html')
