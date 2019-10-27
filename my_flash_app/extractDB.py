cf8c3440afeb1af463b81d2fc0c7a9e91ccd4a7e
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-10-27 16:36:36 -0300

    Python funcionando com html_template_dir

--- my_flash_app/extractDB.py
+++ my_flash_app/extractDB.py
@@ -2,15 +2,14 @@
 import pymysql
 
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 class Database:
     def __init__(self):
         host = "localhost"
         user = "root"
         password = "password"
         db = "manuelDB"
-        self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.
-                                   DictCursor)
+        self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.DictCursor)
         self.cur = self.con.cursor()
     def Idoso(self):
         self.cur.execute("SELECT nomeIdoso, loginIdoso FROM Idoso WHERE idadeIdoso = 70")
@@ -23,4 +22,4 @@ def Idoso():
         oldAge = db.Idoso()
         return oldAge
     res = db_query()
-    return render_template('./idosos.html')
+    return render_template('idosos.html')
